107ac45c114c944d6af491178d17f05bc9700045
kirillreprincev <33598233+kirillreprincev@users.noreply.github.com>
2017-11-28 15:21:31 +0300

    Update and rename cpp to arr.cpp

---
+++ arr.cpp
@@ -0,0 +1,25 @@
+ //обычный массив
+int arr(int x, int y, int z){
+    int a[x];
+    a[0] = 0;
+    while(y <= x){
+        a[y] = a[y-1] + z;
+        cout << a[y] << endl;
+        y++;
+    }
+    int i;
+    int sum = 0;
+    for(i = 0; i <= x; i++){
+        sum = sum + a[i];
+
+
+    } cout << "summ = " << sum << endl;
+
+}
+
+
+int main(){
+ arr(100, 1 ,1);
+    return 0;
+
+}
